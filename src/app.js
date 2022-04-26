b28f8af57332e32e60d9b75c0bb5e5dd09d644f6
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-26 10:54:46 +0200

    GET /products simple

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ const app = express();
 
 app.get('/', (req, res) => res.send('hello'));
 
+app.get('/products', async (req, res) => {
+  try {
+    const [products] = await db.promise().query('SELECT * FROM products');
+    res.send(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
+
 db.connect((err) => {
   if (err) console.error('error connecting to db');
 });
