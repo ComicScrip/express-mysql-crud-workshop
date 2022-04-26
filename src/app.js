204b21195ee924e023dddb3f302c15fd01eb5005
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-26 09:22:28 +0200

    connect to mysql

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 
 app.get('/', (req, res) => res.send('hello'));
+
+db.connect((err) => {
+  if (err) console.error('error connecting to db');
+});
 
 module.exports.app = app;
